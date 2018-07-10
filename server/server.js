076315042c4b68d9f971cb74f3c289ec246264b5
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-07-09 19:00:49 -0700

    Updates gitignore

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const ask = require('./getData');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/directions', async (req, res) => {
+  let directions, endpoints;
+  if (req.query.arri === undefined) {
+    try {
+      [directions, endpoints] = await ask.getNearby(req.query.dept);
+    } catch(err) {
+      res.status(500).send(err);
+    }
+  } else {
+    try {
+      directions = await ask.getDistance(req.query.dept, req.query.arri);
+      endpoints = await ask.getEta(req.query.orig);
+    } catch(err) {
+      res.status(500).send(err);
+    }
+  }
+  res.send({
+    tts: directions.data.rows[0].elements[0],
+    eta: endpoints.data.root.station
+  });
+});
+
+app.listen(3000, () => console.log('server started'));
